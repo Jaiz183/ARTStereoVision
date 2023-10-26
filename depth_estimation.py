1a74afb03500c71ff697ad3e2543fd3882e48f15
Jaiz183 <jaizepic@gmail.com>
2023-10-26 01:53:55 -0400

    Wrote functions to compute depth. Must look at how to compute point cloud later.

---
+++ depth_estimation.py
@@ -0,0 +1,20 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+from common_functions import *
+import math
+
+
+def compute_disparity(matches: list[cv2.DMatch]) -> list[float]:
+    return [match.distance for match in matches]
+
+
+def compute_depth(
+    disparities: list[float], focal_length: float, baseline: float
+) -> list[float]:
+    return [(focal_length * baseline) / disparity for disparity in disparities]
+
+
+def compute_point_cloud():
+    # Implement this!
+    pass
